cb41f3ed7dca8711c335afc490a264aca16d98a8
YYH2913 <yyh2913@qq.com>
2026-08-05 00:26:50 +0800

    refactor: split OpenSSH SFTP integration tests

--- src-tauri/src/tests/mod.rs
+++ src-tauri/src/tests/mod.rs
@@ -57,6 +57,8 @@ mod openssh_jump_host_tests;
 mod openssh_modem_integration;
 #[path = "openssh_reconnect_tests.rs"]
 mod openssh_reconnect_tests;
+#[path = "openssh_sftp_integration.rs"]
+mod openssh_sftp_integration;
 #[path = "openssh_transfer_tunnel_tests.rs"]
 mod openssh_transfer_tunnel_tests;
 #[path = "openssh_tunnel_integration.rs"]
